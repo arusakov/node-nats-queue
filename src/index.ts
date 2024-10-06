880163912adf86a9f077591238c9d650907fed67
Aleksandr Rusakov <aleksandr.s.rusakov@gmail.com>
2024-10-07 02:24:50 +0300

    feat: add Worker class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { nanos, NatsConnection, NatsError, type MsgHdrs } from '@nats-io/nats-core'
+import { nanos, NatsError, type MsgHdrs } from '@nats-io/nats-core'
 
-import type { JetStreamClient } from '@nats-io/jetstream'
-import { jetstreamManager } from '@nats-io/jetstream'
+import { type JetStreamClient, type Consumer, type JsMsg, AckPolicy } from '@nats-io/jetstream'
+import { timeout } from './utils'
 
 
-export type QueueOpts = {
+export type QueueOpts
+ = {
   client: JetStreamClient
   name: string
 
@@ -65,11 +66,100 @@ export class Queue {
     
   }
 
-  async add(name: string, data: unknown, options?: AddOptions) {
+  async add(name: string, data?: unknown, options?: AddOptions) {
     const payload = JSON.stringify(data)
     return this.client.publish(`${this.name}.${name}`, payload, options && {
       msgID: options.id, 
       headers: options.headers
     })
+  }
+}
+
+export type WorkerOpts = {
+  client: JetStreamClient
+  name: string
+  processor: (job: JsMsg) => Promise<void>
+  concurrency?: number
+}
+
+export class Worker {
+  protected readonly client: JetStreamClient
+  protected readonly name: string
+  protected readonly processor: (job: JsMsg) => Promise<void>
+  protected readonly concurrency: number
+
+  protected running = false
+  protected processingNow = 0
+
+
+  constructor(opts: WorkerOpts) {
+    this.client = opts.client
+    this.name = opts.name
+
+    this.processor = opts.processor
+    this.concurrency = opts.concurrency || 1
+  }
+
+  async setup() {
+    const manager = await this.client.jetstreamManager()
+
+    await manager.streams.add({
+      name: this.name,
+      subjects: [`${this.name}.*`], 
+    })
+
+    const consumer = await manager.consumers.add(this.name, {
+      filter_subject: `${this.name}.*`,
+      durable_name: this.name,
+      ack_policy: AckPolicy.All,
+    })
+
+    console.log(consumer)
+  }
+
+  stop() {
+    this.running = false
+  }
+
+  async start() {
+    
+    const consumer = await this.client.consumers.get(this.name, this.name)
+
+    this.running = true
+    do {
+      const jobsForFetch = Math.min(this.concurrency - this.processingNow, this.concurrency)
+
+      const jobs = await this.fetch(consumer, jobsForFetch)
+
+      for await (const j of jobs) {
+        this.process(j) // without await
+      }
+
+      await timeout(150)
+
+    } while (this.running)
+  }
+
+  protected async process(j: JsMsg) {
+    this.processingNow += 1
+    try {
+      this.process(j)
+      await j.ackAck()
+    } catch (e) {
+      await j.term()
+    } finally {
+      this.processingNow -= 1
+    }
+  }
+
+  protected async fetch(consumer: Consumer, count: number) {
+    try {
+      return consumer.fetch({
+        max_messages: count,
+      })
+    } catch (e) {
+      // TODO
+      return []
+    }
   }
 }
--- test/worker.test.ts
+++ test/worker.test.ts
@@ -7,9 +7,9 @@ import { NatsConnection } from '@nats-io/nats-core'
 import type { JetStreamClient, JetStreamManager } from '@nats-io/jetstream'
 
 
-import { DEFAULT_DEDUPLICATE_WINDOW, Queue } from '../src'
+import { Queue, Worker } from '../src'
 
-describe('Queue', () => {
+describe('Worker', () => {
   let connection: NatsConnection
   let client: JetStreamClient
   let manager: JetStreamManager
@@ -25,6 +25,7 @@ describe('Queue', () => {
   })
 
   afterEach(async () => {
+    await manager.consumers.delete(NAME_1)
     await manager.streams.delete(NAME_1)
   })
 
@@ -33,41 +34,32 @@ describe('Queue', () => {
   })
 
   it('create', async () => {
-    const queue1 = new Queue({
+    const worker1 = new Worker({
       client,
       name: NAME_1,
+      processor: async () => {},
     })
-    await queue1.setup()
+    await worker1.setup()
 
-    const stream = await client.streams.get(NAME_1)
+    const consumers = await manager.consumers.list(NAME_1)
 
-    const { config: { name, subjects, duplicate_window }, state: { messages } } = await stream.info()
-
-    equal(name, NAME_1)
-    deepEqual(subjects, [`${name}.*`])
-    equal(duplicate_window, nanos(DEFAULT_DEDUPLICATE_WINDOW))
-    equal(messages, 0)
+    for await (const c of consumers) {
+      console.log(c)
+    }
   })
 
-  it('update', async () => {
-    const queue1 = new Queue({
+  it('create multiple', async () => {
+    const worker1 = new Worker({
       client,
       name: NAME_1,
+      processor: async () => {},
     })
-    await queue1.setup()
+    await worker1.setup()
 
-    const deduplicateWindow = 3_000
-    const queue2 = new Queue({
-      client,
-      name: NAME_1,
-      deduplicateWindow,
-    })
-    await queue2.setup()
+    const consumers = await manager.consumers.list(NAME_1)
 
-    const stream = await client.streams.get(NAME_1)
-
-    const { config: { name, subjects, duplicate_window } } = await stream.info()
-
-    equal(duplicate_window, nanos(deduplicateWindow))
+    for await (const c of consumers) {
+      console.log(c)
+    }
   })
 })
