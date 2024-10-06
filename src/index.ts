d680b27d82475f92d230c672855c12dc842f6eae
Aleksandr Rusakov <aleksandr.s.rusakov@gmail.com>
2024-10-07 00:34:09 +0300

    feat: add Queue class (first iteration)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,75 @@
-import { NatsConnection } from '@nats-io/nats-core'
-import { jetstream, type JetStreamClient, type Consumer, type ConsumerMessages, type JetStreamManager, jetstreamManager } from '@nats-io/jetstream'
+import { nanos, NatsConnection, NatsError, type MsgHdrs } from '@nats-io/nats-core'
+
+import type { JetStreamClient } from '@nats-io/jetstream'
+import { jetstreamManager } from '@nats-io/jetstream'
+
 
 export type QueueOpts = {
-  connection: NatsConnection
+  client: JetStreamClient
   name: string
+
+  /**
+   * ms
+   */
+  deduplicateWindow?: number
 }
 
+export type Job = {
+  id?: number | string
+  name: string
+  data: unknown
+}
+
+export type AddOptions = {
+  id?: string
+  headers?: MsgHdrs
+}
+
+export const DEFAULT_DEDUPLICATE_WINDOW = 2_000
+
 export class Queue {
-  connection: NatsConnection
-  client: JetStreamClient
+  protected readonly client: JetStreamClient
+  protected readonly name: string
+
+  protected readonly deduplicateWindow: number
 
   constructor(opts: QueueOpts) {
-    this.connection = opts.connection
-    this.connection.isClosed
+    this.client = opts.client
+    this.name = opts.name
 
-    this.client = jetstream(this.connection, {})
+    this.deduplicateWindow = opts.deduplicateWindow || DEFAULT_DEDUPLICATE_WINDOW
+  }
+
+  async setup() {
+    const manager = await this.client.jetstreamManager()
+    
+    try {
+      await manager.streams.add({
+        name: this.name,
+        subjects: [`${this.name}.*`],
+        duplicate_window: nanos(this.deduplicateWindow)
+      })
+    } catch (e) {
+      // TODO smart error handling
+      if (!(e instanceof NatsError)) {
+        throw e
+      }
+      await manager.streams.update(
+        this.name,
+        {
+          subjects: [`${this.name}.*`],
+          duplicate_window: nanos(this.deduplicateWindow)
+        }
+      )
+    }
+    
+  }
+
+  async add(name: string, data: unknown, options?: AddOptions) {
+    const payload = JSON.stringify(data)
+    return this.client.publish(`${this.name}.${name}`, payload, options && {
+      msgID: options.id, 
+      headers: options.headers
+    })
   }
 }
--- test/queue.test.ts
+++ test/queue.test.ts
@@ -1,18 +1,73 @@
-import { describe, it, before } from 'node:test'
+import { equal, deepEqual } from 'assert/strict'
+import { describe, it, before, after, afterEach } from 'node:test'
 
-import { NatsConnection } from '@nats-io/nats-core'
-import { connect} from '@nats-io/transport-node'
+import { nanos, NatsConnection } from '@nats-io/nats-core'
+import { connect } from '@nats-io/transport-node'
+
+import { DEFAULT_DEDUPLICATE_WINDOW, Queue } from '../src'
+import { jetstream, type JetStreamClient, type JetStreamManager } from '@nats-io/jetstream'
 
 describe('Queue', () => {
-  let conn: NatsConnection
+  let connection: NatsConnection
+  let client: JetStreamClient
+  let manager: JetStreamManager
+
+  const NAME_1 = 'queue1'
 
   before(async () => {
-    conn = await connect({
+    connection = await connect({
       servers: '127.0.0.1:4222',
     })
+    client = jetstream(connection)
+    manager = await client.jetstreamManager()
   })
 
-  it('creating', () => {
+  afterEach(async () => {
+    await manager.streams.delete(NAME_1)
+  })
 
+  after(async () => {
+    await connection.close()
+  })
+
+  it('create', async () => {
+    const queue1 = new Queue({
+      client,
+      name: NAME_1,
+    })
+
+    await queue1.setup()
+
+    const stream = await client.streams.get(NAME_1)
+
+    const { config: { name, subjects, duplicate_window } } = await stream.info()
+
+    equal(name, NAME_1)
+    deepEqual(subjects, [`${name}.*`])
+    equal(duplicate_window, nanos(DEFAULT_DEDUPLICATE_WINDOW))
+  })
+
+  it('update', async () => {
+    const queue1 = new Queue({
+      client,
+      name: NAME_1,
+    })
+    await queue1.setup()
+
+    const deduplicateWindow = 3_000
+    const queue2 = new Queue({
+      client,
+      name: NAME_1,
+      deduplicateWindow,
+    })
+    await queue2.setup()
+
+    const stream = await client.streams.get(NAME_1)
+
+    const { config: { name, subjects, duplicate_window } } = await stream.info()
+
+    equal(name, NAME_1)
+    deepEqual(subjects, [`${name}.*`])
+    equal(duplicate_window, nanos(deduplicateWindow))
   })
 })
